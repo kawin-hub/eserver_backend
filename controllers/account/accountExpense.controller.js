7b91686945065a28d0ce47501b53f99ec2e1d489
OUM <oum@OUMs-MacBook-Pro.local>
2024-03-06 11:09:25 +0700

    update nodejs eservice_backend by oom 06/03

--- controllers/account/accountExpense.controller.js
+++ controllers/account/accountExpense.controller.js
@@ -8,132 +8,136 @@ const { Validator } = require("node-input-validator");
 // 👉 Get all or by ID
 
 exports.getAccountExpenses = async (req, res) => {
-    var result = new DataResponse();
+  var result = new DataResponse();
 
-    try {
-        const { _id } = req.query;
+  try {
+    const { _id } = req.query;
 
-        var AccountExpenseModel = AccountModel.expense
+    var AccountExpenseModel = AccountModel.expense;
 
-        if (typeof _id != "undefined") {
-            result = await AccountExpenseModel.getAccountExpenseById({
-                _id: new Object(_id),
-            });
-        } else {
-            var pageOption = general.checkPageAndLimit(
-                req.query.page,
-                req.query.limit
-            );
+    if (typeof _id != "undefined") {
+      result = await AccountExpenseModel.getAccountExpenseById({
+        _id: new Object(_id),
+      });
+    } else {
+      var pageOption = general.checkPageAndLimit(
+        req.query.page,
+        req.query.limit
+      );
 
-            var params = {
-                page: pageOption.page,
-                limit: pageOption.limit,
-                queryCondition: {},
-            };
+      var params = {
+        page: pageOption.page,
+        limit: pageOption.limit,
+        queryCondition: {},
+      };
 
-            result = await AccountExpenseModel.getAllAccountExpenses(params);
-        }
-    } catch (error) {
-        console.log(error);
+      result = await AccountExpenseModel.getAllAccountExpenses(params);
     }
+  } catch (error) {
+    console.log(error);
+  }
 
-    res.json(result);
+  res.json(result);
 };
 
 // 👉 Insert/Post
 
 exports.insertAccountExpense = async (req, res) => {
-    var result = new DataResponse();
+  var result = new DataResponse();
 
-    try {
-        var imagesName = "images";
-        var docsName = "documents";
+  try {
+    var imagesName = "images";
+    var docsName = "documents";
 
-        var resUpload = await upload.uploadFiles(req, res, [
-            {
-                name: imagesName,
-                path: "./assets/images/account/expenses",
-                maxCount: 5,
-                allowType: ["jpeg", "jpg", "png"],
-            },
-            {
-                name: docsName,
-                path: "./assets/documents/account/expenses",
-                maxCount: 5,
-                allowType: ["pdf"],
-            },
-        ]);
+    var resUpload = await upload.uploadFiles(req, res, [
+      {
+        name: imagesName,
+        path: "./assets/images/account/expenses",
+        maxCount: 5,
+        allowType: ["jpeg", "jpg", "png"],
+      },
+      {
+        name: docsName,
+        path: "./assets/documents/account/expenses",
+        maxCount: 5,
+        allowType: ["pdf"],
+      },
+    ]);
+    console.log(req.files);
+    if (resUpload.success) {
+      const validation = new Validator(req.body, {
+        documentNumber: "required",
+        expenseDate: "required|dateFormat:YYYY-MM-DD",
+        expenseCategory: "required|in:stock,nonstock",
+        amount: "required",
+        whom: "required",
+      });
+      const matched = await validation.check();
 
-        if (resUpload.success) {
-
-            const validation = new Validator(req.body, {
-                documentNumber: "required",
-                expenseDate: "required|dateFormat:YYYY-MM-DD",
-                expenseCategory: "required|in:stock,nonstock",
-                amount: "required",
-                whom: "required",
-            });
-
-            const matched = await validation.check();
-
-            var AccountExpenseModel = AccountModel.expense
-
-            if (matched) {
-                const { documentNumber, expenseDate, expenseCategory, expenseType, amount, whom, tag, remark } = req.body;
-                var images = []
-                var documents = []
-
-                for (let i = 0; i < req.files[imagesName]?.length; i++) {
-                    images[i] = {
-                        name: req.files[imagesName][i].originalname,
-                        path: req.files[imagesName][i].path
-                    }
-                }
-
-                for (let i = 0; i < req.files[docsName]?.length; i++) {
-                    documents[i] = {
-                        name: req.files[docsName][i].originalname,
-                        path: req.files[docsName][i].path
-                    }
-                }
-                var params = {
-                    documentNumber: documentNumber,
-                    expenseDate: expenseDate,
-                    expenseCategory: typeof expenseCategory != "undefined" ? expenseCategory : "",
-                    expenseType: expenseType,
-                    amount: amount,
-                    whom: whom,
-                    tag: typeof tag != "undefined" ? tag : "",
-                    remark: typeof remark != "undefined" ? remark : "",
-                    images: images,
-                    documents: documents,
-                };
-                result = await AccountExpenseModel.insertAccountExpense(params);
-
-            } else {
-                result.doError(2, validation.errors);
-            }
-
-        } else {
-            result.doError(7, "Files or images are wrong format, please check!");
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
-
-    if (result.code != 1) {
+      var AccountExpenseModel = AccountModel.expense;
+      if (matched) {
+        const {
+          documentNumber,
+          expenseDate,
+          expenseCategory,
+          expenseType,
+          amount,
+          whom,
+          tag,
+          remark,
+        } = req.body;
+        var images = [];
+        var documents = [];
         for (let i = 0; i < req.files[imagesName]?.length; i++) {
-            fs.rmSync(req.files[imagesName][i].path, {
-                force: true,
-            });
+          images[i] = {
+            name: req.files[imagesName][i].originalname,
+            path: req.files[imagesName][i].path,
+          };
         }
-        for (let i = 0; i < req.files[docsName]?.length; i++) {
-            fs.rmSync(req.files[docsName][i].path, {
-                force: true,
-            });
-        }
-    }
 
-    res.json(result);
-}
+        for (let i = 0; i < req.files[docsName]?.length; i++) {
+          documents[i] = {
+            name: req.files[docsName][i].originalname,
+            path: req.files[docsName][i].path,
+          };
+        }
+        var params = {
+          documentNumber: documentNumber,
+          expenseDate: expenseDate,
+          expenseCategory:
+            typeof expenseCategory != "undefined" ? expenseCategory : "",
+          expenseType: expenseType,
+          amount: amount,
+          whom: whom,
+          tag: typeof tag != "undefined" ? tag : "",
+          remark: typeof remark != "undefined" ? remark : "",
+          images: images,
+          documents: documents,
+        };
+        result = await AccountExpenseModel.insertAccountExpense(params);
+      } else {
+        result.doError(2, validation.errors);
+      }
+      console.log(validation.errors);
+    } else {
+      result.doError(7, "Files or images are wrong format, please check!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (result.code != 1) {
+    for (let i = 0; i < req.files[imagesName]?.length; i++) {
+      fs.rmSync(req.files[imagesName][i].path, {
+        force: true,
+      });
+    }
+    for (let i = 0; i < req.files[docsName]?.length; i++) {
+      fs.rmSync(req.files[docsName][i].path, {
+        force: true,
+      });
+    }
+  }
+
+  res.json(result);
+};
--- controllers/sale/saleLead.controller.js
+++ controllers/sale/saleLead.controller.js
@@ -10,86 +10,99 @@ dotenv.config();
 // 👉 Get all or by ID
 
 exports.getSaleLeads = async (req, res) => {
-    var result = new DataResponse();
+  var result = new DataResponse();
 
-    try {
-        const { _id } = req.query;
+  try {
+    const { _id } = req.query;
 
-        var SaleLeadModel = SaleModel.lead
+    var SaleLeadModel = SaleModel.lead;
 
-        if (typeof _id != "undefined") {
-            result = await SaleLeadModel.getSaleLeadById({
-                _id: new Object(_id),
-            });
-        } else {
-            var pageOption = general.checkPageAndLimit(
-                req.query.page,
-                req.query.limit
-            );
+    if (typeof _id != "undefined") {
+      result = await SaleLeadModel.getSaleLeadById({
+        _id: new Object(_id),
+      });
+    } else {
+      var pageOption = general.checkPageAndLimit(
+        req.query.page,
+        req.query.limit
+      );
 
-            var params = {
-                page: pageOption.page,
-                limit: pageOption.limit,
-                queryCondition: {},
-            };
+      var params = {
+        page: pageOption.page,
+        limit: pageOption.limit,
+        queryCondition: {},
+      };
 
-            result = await SaleLeadModel.getAllSaleLeads(params);
-        }
-    } catch (error) {
-        console.log(error);
+      result = await SaleLeadModel.getAllSaleLeads(params);
+      console.log(result.data.documents);
     }
+  } catch (error) {
+    console.log(error);
+  }
 
-    res.json(result);
+  res.json(result);
 };
 
 // 👉 Insert/Post
 
 exports.insertSaleLead = async (req, res) => {
+  var result = new DataResponse();
 
-    var result = new DataResponse();
+  try {
+    const validation = new Validator(req.body, {
+      companyName: "required",
+      leadFirstname: "required",
+      leadContactNumber: "required",
+      leadLevel: "required|in:low prudential,middle prudential,high prudential",
+    });
 
-    try {
+    const matched = await validation.check();
 
-        const validation = new Validator(req.body, {
-            companyName: "required",
-            leadFirstname: "required",
-            leadContactNumber: "required",
-            leadLevel: "required|in:low prudential,middle prudential,high prudential",
-        });
+    var SaleLeadModel = SaleModel.lead;
 
-        const matched = await validation.check();
+    if (matched) {
+      const {
+        companyName,
+        taxId,
+        branch,
+        address,
+        googleMap,
+        companyEmail,
+        companyContactNumber,
+        leadFirstname,
+        leadLastname,
+        leadContactNumber,
+        lineId,
+        leadLevel,
+        tag,
+      } = req.body;
 
-        var SaleLeadModel = SaleModel.lead
+      var inserLeadparams = {
+        companyName: companyName,
+        taxId: typeof taxId != "undefined" ? taxId : "",
+        branch: typeof branch != "undefined" ? branch : "",
+        address: typeof address != "undefined" ? address : "",
+        googleMap: typeof googleMap != "undefined" ? googleMap : "",
+        companyEmail: typeof companyEmail != "undefined" ? companyEmail : "",
+        companyContactNumber:
+          typeof companyContactNumber != "undefined"
+            ? companyContactNumber
+            : "",
+        leadFirstname: leadFirstname,
+        leadLastname: typeof leadLastname != "undefined" ? leadLastname : "",
+        leadContactNumber: leadContactNumber,
+        lineId: typeof lineId != "undefined" ? lineId : "",
+        leadLevel: leadLevel,
+        tag: typeof tag != "undefined" ? tag : "",
+      };
 
-        if (matched) {
-
-            const { companyName, taxId, branch, address, googleMap, companyEmail, companyContactNumber, leadFirstname, leadLastname, leadContactNumber, lineId, leadLevel, tag } = req.body;
-            
-            var inserLeadparams = {
-                companyName: companyName,
-                taxId: typeof taxId != "undefined" ? taxId : "",
-                branch: typeof branch != "undefined" ? branch : "",
-                address: typeof address != "undefined" ? address : "",
-                googleMap: typeof googleMap != "undefined" ? googleMap : "",
-                companyEmail: typeof companyEmail != "undefined" ? companyEmail : "",
-                companyContactNumber: typeof companyContactNumber != "undefined" ? companyContactNumber : "",
-                leadFirstname: leadFirstname,
-                leadLastname: typeof leadLastname != "undefined" ? leadLastname : "",
-                leadContactNumber: leadContactNumber,
-                lineId: typeof lineId != "undefined" ? lineId : "",
-                leadLevel: leadLevel,
-                tag: typeof tag != "undefined" ? tag : "",
-            };
-
-            result = await SaleLeadModel.insertSaleLead(inserLeadparams);
-
-        } else {
-            result.doError(2, validation.errors);
-        }
-
-    } catch (error) {
-        console.log(error)
+      result = await SaleLeadModel.insertSaleLead(inserLeadparams);
+    } else {
+      result.doError(2, validation.errors);
     }
+  } catch (error) {
+    console.log(error);
+  }
 
-    res.json(result);
-}
+  res.json(result);
+};
